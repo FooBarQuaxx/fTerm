4a652be76b8d27f6a420efb57f77dd7a861d66ea
Liam Schumm <liamschumm@icloud.com>
2016-08-07 12:58:56 -0500

    - Added nouns to parser - Added "home" and "root" nouns: ~ and /, respectively

--- words/parser.py
+++ words/parser.py
@@ -12,7 +12,7 @@ from difflib import get_close_matches
 
 # import words
 from verbs import verbs
-# from nouns import nouns
+from nouns import nouns
 
 # for synonym matching
 meanings = {}
@@ -24,7 +24,7 @@ for item in open(expanduser("~/fTerm-dev/words/meanings.p"), "r").read().split("
 
 
 words = verbs.keys()
-# words.append(nouns.keys())
+words.append(nouns.keys())
 
 def parse(word):
     """Interpret word as a fTerm word."""
@@ -39,6 +39,21 @@ def parse(word):
             raise KeyError
         else:
             return lookup[0]
+
+def softParse(word):
+    """Interpret noun softly (i.e., see if it's defined, if not, just pass it as an argument)."""
+    if word in words:
+        return word
+    elif meanings[word] != []:
+        return meanings[word]
+    else:
+        lookup = get_close_matches(word, words)
+        if len(lookup) == 0:
+            # there aren't any reasonable matches
+            return word
+        else:
+            return lookup[0]
+
 
 def printAndParse(word):
     """Print and parse"""
