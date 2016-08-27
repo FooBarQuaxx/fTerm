43182c8f217c234a76a62d44ea76210acd77c2e3
Liam Schumm <liamschumm@icloud.com>
2016-08-27 10:27:23 -0500

    Update homebrew version tag

--- Formula/fterm.rb
+++ Formula/fterm.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Fterm < Formula
   homepage 'https://github.com/lschumm/homebrew-fTerm/'
   url 'https://github.com/lschumm/homebrew-fTerm.git'
-  version '0.0.2a5'
+  version '1.2.0'
 
   depends_on "xz"
   depends_on "pxz"
