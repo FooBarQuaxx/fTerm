f28741797f23f180b35446fb1e912e2e95716321
Liam Schumm <liamschumm@icloud.com>
2016-09-11 08:03:45 -0500

    - Remove unused f interactive shell - Update home-brew version tag

--- Formula/fterm.rb
+++ Formula/fterm.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Fterm < Formula
   homepage 'https://github.com/lschumm/homebrew-fTerm/'
   url 'https://github.com/lschumm/homebrew-fTerm.git'
-  version '1.2.0b5'
+  version '2.0.0b1'
 
   depends_on "xz"
   depends_on "pxz"
