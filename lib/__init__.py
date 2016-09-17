6acbd4874d72cae29c9ac31f4ae2d9ea9abb460b
Liam Schumm <liamschumm@icloud.com>
2016-09-17 09:37:41 -0500

    Make __init__.py not import dead symlinks

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,11 @@
 """
-[fTerm] verbs.py
+[fTerm] __init__.py
 
 This module aggregates all of the modules in lib/ to be imported by load.py.
 """
 
 import os
 for module in os.listdir(os.path.dirname(__file__)):
-    if module == '__init__.py' or module[-3:] != '.py':
+    if module == '__init__.py' or module[-3:] != '.py' or not os.path.exists(module):
         continue
     __import__(module[:-3], locals(), globals())
